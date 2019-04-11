0b280fa65c3f16bae5a6c9a4cecc94c7686cf98b
billy-mcgrath <william.f.mcgrath.20@dartmouth.edu>
2019-04-11 13:02:08 -0400

    adding all of the files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let count = 0;
+
+function increment() {
+  $('#main').html(`You've been on this page for ${count} seconds.`);
+  count += 1;
+}
+
+setInterval(increment, 1000);
