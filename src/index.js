54ce0d4f5fcd7356e4ea834345cd021ef8d9a030
billy-mcgrath <william.f.mcgrath.20@dartmouth.edu>
2019-04-15 09:47:02 -0400

    adding initial changes to support React

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-// change require to es6 import style
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let count = 0;
+const App = () => <div className="test">All the REACT are belong to us!</div>;
 
-function increment() {
-  $('#main').html(`You've been on this page for ${count} seconds.`);
-  count += 1;
-}
-
-setInterval(increment, 1000);
+ReactDOM.render(<App />, document.getElementById('main'));
